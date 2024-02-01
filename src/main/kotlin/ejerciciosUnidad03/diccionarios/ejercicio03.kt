d6a9501d51c7a1ec85a42c9742449859bef9534b
Sara S Camilleri <ssancam391@g.educaand.es>
2024-02-01 15:56:56 +0100

    primero_diccionarios

--- src/main/kotlin/ejerciciosUnidad03/diccionarios/ejercicio03.kt
+++ src/main/kotlin/ejerciciosUnidad03/diccionarios/ejercicio03.kt
@@ -1,2 +1,53 @@
-package org.example.ejerciciosUnidad03.diccionarios
+package ejerciciosUnidad03.diccionarios
 
+/*
+Escribir un programa que guarde en un diccionario los precios de las frutas de la tabla, pregunte al usuario por una
+fruta, un número de kilos y muestre por pantalla el precio de ese número de kilos de fruta.
+Si la fruta no está en el diccionario debe mostrar un mensaje informando de ello.
+
+Fruta	Precio
+Plátano	1.35
+Manzana	0.80
+Pera	0.85
+Naranja	0.70
+*/
+/*
+fun calcularPrecio(kilos: Double, precio: Double): Double{
+
+}
+
+ */
+fun main() {
+    val stock = mapOf("PLATANO" to 1.35, "MANZANA" to 0.80, "PERA" to 0.85, "NARANJA" to 0.70)
+    val carrito = mutableListOf<String>()
+    var articulo = ""
+
+    println("En nuestra tienda puedes encontrar: ")
+    for (fruta in stock) {
+        println(fruta)
+    }
+    var seguir = true
+    while (seguir) {
+        println("Indica un producto que desees comprar: ")
+        println("Fruta>")
+        articulo = readln().uppercase()
+
+        if (articulo.uppercase() !in stock.keys) {
+            println("No disponemos de ese articulo.\nInténtalo de nuevo: ")
+            articulo = readln().uppercase()
+        } else {
+            carrito.add(articulo)
+        }
+        println("Quieres introducir otra fruta al carrito? S/N: ")
+        val continuar = readln().uppercase()
+
+        if (continuar == "N") {
+            seguir = false
+        } else if (continuar == "S") {
+            continue
+        } else {
+            println("Debes introducir S o N: ")
+        }
+    }
+    //HEMOS SALIDO Y QUEREMOS CALCULAR EL TOTAL DEL CARRITO:
+}
