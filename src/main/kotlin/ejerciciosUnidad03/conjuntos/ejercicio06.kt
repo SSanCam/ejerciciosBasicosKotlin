28bb5f4e8a70970954879d3c71de158f24a08b88
Sara S Camilleri <ssancam391@g.educaand.es>
2024-02-21 15:36:15 +0100

    enunciados_restantes_agregados

--- src/main/kotlin/ejerciciosUnidad03/conjuntos/ejercicio06.kt
+++ src/main/kotlin/ejerciciosUnidad03/conjuntos/ejercicio06.kt
@@ -1,2 +1,8 @@
-package org.example.ejerciciosUnidad03.conjuntos
+package ejerciciosUnidad03.conjuntos
+/*
+Dado el conjunto de letras:
 
+vocales = {'a', 'e', 'i', 'o', 'u'}
+Crea un conjunto consonantes que contenga las letras del alfabeto que no son vocales.
+Crea un conjunto letras_comunes que contenga las letras que están tanto en el conjunto vocales como en el conjunto consonantes.
+ */
