c07d8f34b51deb9741046714a9569732ced18de5
Sara S Camilleri <ssancam391@g.educaand.es>
2024-03-04 10:40:04 +0100

    conjuntos_05_completo

--- src/main/kotlin/ejerciciosUnidad03/conjuntos/ejercicio05.kt
+++ src/main/kotlin/ejerciciosUnidad03/conjuntos/ejercicio05.kt
@@ -8,3 +8,18 @@ Crea un conjunto multiplos_de_tres que contenga los números que son múltiplos 
 Encuentra la intersección entre los conjuntos pares y multiplos_de_tres y guárdala en un conjunto llamado pares_y_multiplos_de_tres.
  */
 
+fun main () {
+    val numeros = setOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    // Sacamos los números pares del set numeros
+    val numerosPares = numeros.filter { it % 2 == 0  }.toSet()
+    // Sacamos los múltiplos de tres del conjunto numeros
+    val multiplosDeTres = numeros.filter { it % 3 == 0 }.toSet()
+    // Sacamos los números pares que sean múltiplo de 3
+    val paresMultiplosTres = multiplosDeTres.intersect(numerosPares)
+
+    println("Dado el conjunto de números: $numeros")
+    println()
+    println("Los números pares son: $numerosPares")
+    println("Los números múltiplos de 3 son: $multiplosDeTres")
+    println("Los números múltiplos de 3 que son pares son: $paresMultiplosTres")
+}
