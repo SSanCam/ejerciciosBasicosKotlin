c804b7e24b43037139ae351864a80863960c7ae4
Sara S Camilleri <ssancam391@g.educaand.es>
2024-03-04 10:03:17 +0100

    conjuntos_04_hecho

--- src/main/kotlin/ejerciciosUnidad03/conjuntos/ejercicio04.kt
+++ src/main/kotlin/ejerciciosUnidad03/conjuntos/ejercicio04.kt
@@ -1,11 +1,33 @@
 package ejerciciosUnidad03.conjuntos
 /*
 Dadas las siguientes listas:
+
 frutas1 = ["manzana", "pera", "naranja", "plátano", "uva"]
 frutas2 = ["manzana", "pera", "durazno", "sandía", "uva"]
+
 Crea conjuntos a partir de estas listas y nómbralos set_frutas1 y set_frutas2.
+
 Encuentra las frutas que están en ambas listas y guárdalas en un nuevo conjunto llamado frutas_comunes.
 Encuentra las frutas que están en frutas1 pero no en frutas2 y guárdalas en un conjunto llamado frutas_solo_en_frutas1.
 Encuentra las frutas que están en frutas2 pero no en frutas1 y guárdalas en un conjunto llamado frutas_solo_en_frutas2.
  */
 
+fun main() {
+
+    val frutas1 = listOf("manzana", "pera", "naranja", "plátano", "uva")
+    val frutas2 = listOf("manzana", "pera", "durazno", "sandía", "uva")
+
+    val setFrutas1 = frutas1.toSet()
+    val setFrutas2 = frutas2.toSet()
+
+    val frutasComunes = setFrutas1.intersect(setFrutas2)
+    val soloFrutasEn1 = setFrutas1.minus(frutas2)
+    val soloFrutasEn2 = setFrutas2.minus(frutas1)
+
+    println("Frutas del set 1: $setFrutas1")
+    println("Frutas en el ser 2: $setFrutas2")
+    println()
+    println("Frutas comunes: $frutasComunes")
+    println("Frutas1 que no aparecen en frutas2: $soloFrutasEn1")
+    println("Frutas2 que no aparecen en frutas1: $soloFrutasEn2")
+}
