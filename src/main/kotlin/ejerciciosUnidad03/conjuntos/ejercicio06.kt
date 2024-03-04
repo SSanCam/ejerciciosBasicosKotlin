321d6d235afb8afbd076b10ff290ce3d9bdc9416
Sara S Camilleri <ssancam391@g.educaand.es>
2024-03-04 10:49:07 +0100

    conjuntos_06_terminado

--- src/main/kotlin/ejerciciosUnidad03/conjuntos/ejercicio06.kt
+++ src/main/kotlin/ejerciciosUnidad03/conjuntos/ejercicio06.kt
@@ -6,3 +6,16 @@ vocales = {'a', 'e', 'i', 'o', 'u'}
 Crea un conjunto consonantes que contenga las letras del alfabeto que no son vocales.
 Crea un conjunto letras_comunes que contenga las letras que están tanto en el conjunto vocales como en el conjunto consonantes.
  */
+fun main () {
+
+    val vocales = setOf('a','e','i','o','u')
+    val abecedario = 'a'..'z'
+    val consonantes = abecedario.minus(vocales)
+    val letrasComunes = vocales.intersect(consonantes.toSet())
+
+    println("Abecedario: $abecedario")
+    println()
+    println("Vocales: $vocales")
+    println("Consonantes: $consonantes")
+    println("Letras comunes: $letrasComunes")
+}
